b6f2479d5db5f005fbb9778a0c3df58b97572d67
ybello02 <56275192+ybello02@users.noreply.github.com>
2020-08-01 00:09:30 -0400

    Update fwknopSetup.sh

--- gateway/fwknopSetup.sh
+++ gateway/fwknopSetup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Copying config files..."
-yes Y  | cp access.conf /etc/fwknop/
-yes Y  | cp fwknopd.conf /etc/fwknop/
-yes Y  | cp gate_sdp_ctrl_client.conf /etc/fwknop/
-yes Y  | cp gate.fwknoprc /etc/fwknop/
-
 while true; do
     sleep 100
 done
