127c1dc97e418ea8bc8d3ecb4158c234911d8b96
ybello02 <56275192+ybello02@users.noreply.github.com>
2020-10-18 13:33:45 -0400

    Update Gencert.sh

--- controller/Gencert.sh
+++ controller/Gencert.sh
@@ -17,9 +17,9 @@ openssl req -new -x509 -days 365 -key ca.key -out ca.crt -passin pass:$password 
     -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 
 service mysql start
-sudo node ./SDPController/genCredentials.js 1
-sudo node ./SDPController/genCredentials.js 2
-sudo node ./SDPController/genCredentials.js 3
+node ./SDPController/genCredentials.js 1
+node ./SDPController/genCredentials.js 2
+node ./SDPController/genCredentials.js 3
 
 #Automate numbner of SDP component credential to generate
 #sdpComponentCount=3
